02b8dab2b3bde20a14634a64d11ff8ffefd8db6b
leohidalgo <leohidalgo@protonmail.com>
2025-01-01 16:08:58 -0300

    feat(aoc): add time execution output

--- Sources/AOC/main.swift
+++ Sources/AOC/main.swift
@@ -11,6 +11,10 @@ struct AOC: ParsableCommand {
     var year: Int = 2024
 
     func run() throws {
+        let t1 = Date()
         print("Run puzzle day '\(day)' year '\(year)'")
+        let t2 = Date()
+
+        print("Time \(String(format: "%.3f", t2.timeIntervalSince(t1)))s")
     }
 }
