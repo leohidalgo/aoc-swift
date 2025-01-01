ad9704a0b37155daf33c3e5216f78b080a505b36
leohidalgo <leohidalgo@protonmail.com>
2025-01-01 16:06:13 -0300

    feat(aoc): add argument parser cli

--- Package.swift
+++ Package.swift
@@ -4,7 +4,18 @@ import PackageDescription
 
 let package = Package(
     name: "aoc",
+    products: [
+        .executable(name: "aoc", targets: ["AOC"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0")
+    ],
     targets: [
-        .executableTarget(name: "AOC"),
+        .executableTarget(
+            name: "AOC",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]
+        )
     ]
 )
--- Sources/AOC/main.swift
+++ Sources/AOC/main.swift
@@ -1,3 +1,16 @@
+import ArgumentParser
 import Foundation
 
-print("Hello, world!")
+@main
+struct AOC: ParsableCommand {
+
+    @Option
+    var day: Int
+
+    @Option
+    var year: Int = 2024
+
+    func run() throws {
+        print("Run puzzle day '\(day)' year '\(year)'")
+    }
+}
