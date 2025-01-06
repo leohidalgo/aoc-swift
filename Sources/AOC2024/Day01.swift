f1dc9a2dc3ae3dfd042b2b5ebb766a9ec4178ae9
leohidalgo <leohidalgo@protonmail.com>
2025-01-06 14:54:10 -0300

    feat(aoc-2024): add Day 1

--- Sources/AOC2024/Day01.swift
+++ Sources/AOC2024/Day01.swift
@@ -3,14 +3,33 @@ import Foundation
 
 struct Day01: Day {
 
-    let title = "Foo Puzzle"
+    let title = "Historian Hysteria"
     var rawInput: String?
 
     func part1() throws -> Int {
-        return -1
+        var left = input().lines.map(\.integers[0])
+        var right = input().lines.map(\.integers[1])
+
+        left.sort()
+        right.sort()
+
+        return zip(left, right)
+            .map(-)
+            .map(abs)
+            .sum
     }
 
     func part2() throws -> Int {
-        return -1
+        let left = input().lines.map(\.integers[0])
+        let right = input().lines.map(\.integers[1])
+
+        let counter = right
+            .reduce(into: [:]) { result, element in
+                result[element, default: 0] += 1
+            }
+
+        return left
+            .map { counter[$0, default: 0] * $0 }
+            .sum
     }
 }
--- Tests/AOCTests/AOC2024Tests.swift
+++ Tests/AOCTests/AOC2024Tests.swift
@@ -7,5 +7,17 @@ struct AOC2024Tests {
 
     @Test
     func day01() throws {
+        var sut = Day01()
+        sut.rawInput = """
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
+        """
+
+        #expect(try sut.part1() == 11, "Part 1")
+        #expect(try sut.part2() == 31, "Part 2")
     }
 }
