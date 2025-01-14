c18b32a0a968e759c10b7bf298c54b090b8f8b54
leohidalgo <leohidalgo@protonmail.com>
2025-01-14 18:49:18 -0300

    feat(aoc-core): add Int character

--- Sources/AOCCore/Int.swift
+++ Sources/AOCCore/Int.swift
@@ -5,4 +5,24 @@ public extension Int {
     var signum: Int {
         signum()
     }
+
+    init?(_ character: Character) {
+        guard
+            character.isNumber
+        else { return nil }
+
+        switch character {
+            case "0": self = 0
+            case "1": self = 1
+            case "2": self = 2
+            case "3": self = 3
+            case "4": self = 4
+            case "5": self = 5
+            case "6": self = 6
+            case "7": self = 7
+            case "8": self = 8
+            case "9": self = 9
+            default: return nil
+        }
+    }
 }
