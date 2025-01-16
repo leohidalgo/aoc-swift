cc680d25cf540537e88000f3e3d0d30f9aad39ba
leohidalgo <leohidalgo@protonmail.com>
2025-01-15 23:33:53 -0300

    feat(aoc-2024): add Day 09 (part 2) (#35)

--- Sources/AOC2024/Day09.swift
+++ Sources/AOC2024/Day09.swift
@@ -8,18 +8,7 @@ struct Day09: Day {
 
     func part1() throws -> Int {
         let diskMap = input().characters.compactMap(Int.init)
-
-        var disk = diskMap
-            .enumerated()
-            .reduce(into: (disk: [], id: 0)) { result, item in
-                if item.offset.isEven {
-                    result.disk.append(contentsOf: Array(repeating: Disk.file(id: result.id), count: item.element))
-                    result.id += 1
-                } else {
-                    result.disk.append(contentsOf: Array(repeating: Disk.free, count: item.element))
-                }
-            }
-            .disk as [Disk]
+        var disk = makeDiskPart1(diskMap)
 
         var left = 0
         var right = disk.count - 1
@@ -30,15 +19,58 @@ struct Day09: Day {
             disk.swapAt(left, right)
         }
 
-        return disk
-            .compactMap(\.fileId)
-            .enumerated()
-            .map(*)
-            .sum
+        return disk.checksum()
     }
 
     func part2() throws -> Int {
-        return -1
+        let diskMap = input().characters.compactMap(Int.init)
+        var (files, free) = makeDiskPart2(diskMap)
+
+        for id in (0..<files.count).reversed() {
+            guard
+                let (fileIndex, fileSize) = files[id],
+                let i = free.firstIndex(where: { $0.size >= fileSize })
+            else { continue }
+
+            if fileIndex > free[i].index {
+                files[id]?.index = free[i].index
+                free[i].index += fileSize
+                free[i].size -= fileSize
+            }
+        }
+
+        return files.checksum()
+    }
+
+    private func makeDiskPart1(_ diskMap: [Int]) -> [Disk] {
+        diskMap
+            .enumerated()
+            .reduce(into: (disk: [], id: 0)) { result, item in
+                if item.offset.isEven {
+                    result.disk.append(contentsOf: Array(repeating: Disk.file(id: result.id), count: item.element))
+                    result.id += 1
+                } else {
+                    result.disk.append(contentsOf: Array(repeating: Disk.free, count: item.element))
+                }
+            }
+            .disk as [Disk]
+    }
+
+    private func makeDiskPart2(_ diskMap: [Int]) -> (files: [Int: Block], free: [Block]) {
+        let (files, free, _, _) = diskMap
+            .enumerated()
+            .reduce(into: (files: Dictionary<Int, Block>(), free: Array<Block>(), id: 0, index: 0)) { result, item in
+                if item.offset.isEven {
+                    result.files[result.id] = (result.index, item.element)
+                    result.id += 1
+                } else {
+                    result.free.append((result.index, item.element))
+                }
+
+                result.index += item.element
+            }
+
+        return (files, free)
     }
 }
 
@@ -64,5 +96,30 @@ private enum Disk: CustomDebugStringConvertible {
             case let .file(id): "\(id)"
             case .free: "."
         }
+    }
+}
+
+private extension Dictionary where Key == Int, Value == Block {
+
+    func checksum() -> Int {
+        self
+            .map {
+                let startIndex = $0.value.index
+                let endIndex = $0.value.index + $0.value.size
+
+                return (startIndex..<endIndex).sum * $0.key
+            }
+            .sum
+    }
+}
+
+private extension Collection where Element == Disk {
+
+    func checksum() -> Int {
+        self
+            .compactMap(\.fileId)
+            .enumerated()
+            .map(*)
+            .sum
     }
 }
