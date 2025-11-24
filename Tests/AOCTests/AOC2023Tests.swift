622d12a4f19e1bfe3cac9dc5a2a76c65b44904a3
leohidalgo <leohidalgo@protonmail.com>
2025-11-24 18:47:09 -0300

    feat(aoc-2023): add Day 01 (#43)

--- Tests/AOCTests/AOC2023Tests.swift
+++ Tests/AOCTests/AOC2023Tests.swift
@@ -5,9 +5,30 @@ import Testing
 struct AOC2023Tests {
     @Test
     func day01() throws {
-        let sut = Day01()
+        var sut = Day01()
 
-        #expect(try sut.part1() == -1, "Part 1")
-        #expect(try sut.part2() == -1, "Part 2")
+        sut.rawInput = """
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+        """
+        #expect(try sut.part1() == 142, "Part 1")
+
+        sut.rawInput = """
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+        """
+        #expect(try sut.part2() == 281, "Part 2")
+
+        sut.rawInput = """
+        twone
+        """
+        #expect(try sut.part2() == 21, "Part 2b")
     }
 }
