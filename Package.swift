6e3a784cfdcf9de4e386904017e5c8a6fd082eb4
leohidalgo <leohidalgo@protonmail.com>
2025-11-24 11:35:35 -0300

    build: update swift-argument-parser to v1.6.2 (#38)

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .executable(name: "aoc", targets: ["AOC"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.6.2")
     ],
     targets: [
         .target(name: "AOC2024", dependencies: ["AOCCore"]),
