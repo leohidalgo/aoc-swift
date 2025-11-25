ba0fdff76b15c3358e1a6a3149b6b9b41016c52d
leohidalgo <leohidalgo@protonmail.com>
2025-11-25 10:22:33 -0300

    feat(aoc-2023): add Day02 (part 2)

--- Tests/AOCTests/AOC2023Tests.swift
+++ Tests/AOCTests/AOC2023Tests.swift
@@ -35,6 +35,7 @@ struct AOC2023Tests {
     @Test
     func day02() throws {
         var sut = Day02()
+
         sut.rawInput = """
         Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
         Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
@@ -42,7 +43,15 @@ struct AOC2023Tests {
         Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
         Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
         """
-
         #expect(try sut.part1() == 8, "Part 1")
+
+        sut.rawInput = """
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        """
+        #expect(try sut.part2() == 2286, "Part 2")
     }
 }
