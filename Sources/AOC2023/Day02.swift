33802980ad61c214456414e307ec25da0105f286
leohidalgo <leohidalgo@protonmail.com>
2025-11-26 09:54:21 -0300

    feat(aoc-2023): add Day02 (part 2) (#46)

--- Sources/AOC2023/Day02.swift
+++ Sources/AOC2023/Day02.swift
@@ -7,34 +7,15 @@ struct Day02: Day {
     var rawInput: String?
 
     func part1() throws -> Int {
-        let query = Regex {
-            TryCapture.integer
-            OneOrMore(.whitespace)
-            TryCapture {
-                ChoiceOf {
-                    "blue"
-                    "red"
-                    "green"
-                }
-            } transform: { String($0) }
-        }
-
         let limits = ["red": 12, "green": 13, "blue": 14]
 
         return input().lines
-            .map(\.raw)
-            .map {
-                $0
-                    .split(separator: ";")
-                    .allSatisfy { group in
-                        let total = group
-                            .matches(of: query)
-                            .reduce(into: [:], { result, element in
-                                result[element.output.2, default: 0] += element.output.1
-                            })
-
-                        return limits.allSatisfy { color, maxAllowed in
-                            total[color, default: 0] <= maxAllowed
+            .map { line in
+                line
+                    .subsets()
+                    .allSatisfy { subset in
+                        limits.allSatisfy { color, maxAllowed in
+                            subset[color, default: 0] <= maxAllowed
                         }
                     }
             }
@@ -47,6 +28,46 @@ struct Day02: Day {
     }
 
     func part2() throws -> Int {
-        -1
+        input().lines
+            .map { line in
+                let maxValues = line
+                    .subsets()
+                    .reduce((red: 0, green: 0, blue: 0)) { result, subset in
+                        (
+                            red: max(result.red, subset["red", default: 0]),
+                            green: max(result.green, subset["green", default: 0]),
+                            blue: max(result.blue, subset["blue", default: 0])
+                        )
+                    }
+
+                return maxValues.red * maxValues.green * maxValues.blue
+            }
+            .sum
+    }
+}
+
+private extension Line {
+    func subsets() -> [[String: Int]] {
+        let query = Regex {
+            TryCapture.integer
+            OneOrMore(.whitespace)
+            TryCapture {
+                ChoiceOf {
+                    "blue"
+                    "red"
+                    "green"
+                }
+            } transform: { String($0) }
+        }
+
+        return self.raw
+            .split(separator: ";")
+            .map { group in
+                group
+                .matches(of: query)
+                .reduce(into: [:], { result, element in
+                    result[element.output.2, default: 0] += element.output.1
+                })
+            }
     }
 }
