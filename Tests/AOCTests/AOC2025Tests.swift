c4be3c44e89427c6e1e2c449bee57d56d1665805
leohidalgo <leohidalgo@protonmail.com>
2025-12-01 12:30:15 -0300

    feat(aoc-2025): add Day 01

--- Tests/AOCTests/AOC2025Tests.swift
+++ Tests/AOCTests/AOC2025Tests.swift
@@ -5,9 +5,21 @@ import Testing
 struct AOC2025Tests {
     @Test
     func day01() throws {
-        let sut = Day01()
+        var sut = Day01()
+        sut.rawInput = """
+        L68
+        L30
+        R48
+        L5
+        R60
+        L55
+        L1
+        L99
+        R14
+        L82
+        """
 
-        #expect(try sut.part1() == -1, "Part 1")
-        #expect(try sut.part2() == -1, "Part 2")
+        #expect(try sut.part1() == 3, "Part 1")
+        #expect(try sut.part2() == 6, "Part 2")
     }
 }
