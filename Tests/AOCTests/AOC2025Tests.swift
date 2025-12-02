9eacad24c87b13b2d0c530df6ccf8c0355ef0848
leohidalgo <leohidalgo@protonmail.com>
2025-12-02 19:09:59 -0300

    feat(aoc-2025): add Day 02 (#51)

--- Tests/AOCTests/AOC2025Tests.swift
+++ Tests/AOCTests/AOC2025Tests.swift
@@ -22,4 +22,17 @@ struct AOC2025Tests {
         #expect(try sut.part1() == 3, "Part 1")
         #expect(try sut.part2() == 6, "Part 2")
     }
+
+    @Test
+    func day02() throws {
+        var sut = Day02()
+        // swiftlint:disable line_length
+        sut.rawInput = """
+        11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+        """
+        // swiftlint:enable line_length
+
+        #expect(try sut.part1() == 1_227_775_554, "Part 1")
+        #expect(try sut.part2() == 4_174_379_265, "Part 2")
+    }
 }
