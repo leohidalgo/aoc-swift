9d7c2f1593b65eca6965502e49ae7dbe6ed1fe8e
leohidalgo <leohidalgo@protonmail.com>
2025-12-05 15:12:30 -0300

    feat(aoc-2025): add Day 05 (part 2)

--- Sources/AOC2025/Day05.swift
+++ Sources/AOC2025/Day05.swift
@@ -12,17 +12,38 @@ struct Day05: Day {
         let ingredients = input().sections[1].lines
             .flatMap(\.integers)
 
-        return ingredients.filter { id in
-            for range in ranges where range.contains(id) {
-                return true
-            }
+        return ingredients
+            .filter { id in
+                for range in ranges where range.contains(id) {
+                    return true
+                }
 
-            return false
-        }
-        .count
+                return false
+            }
+            .count
     }
 
     func part2() throws -> Int {
-        -1
+        input().sections[0].lines
+            .map(\.integers)
+            .map { $0[0]...$0[1] }
+            .sorted { $0.lowerBound < $1.lowerBound }
+            .reduce(into: [ClosedRange<Int>]()) { result, range in
+                guard
+                    let last = result.last
+                else {
+                    result.append(range)
+                    return
+                }
+
+                if last.overlaps(range) {
+                    result.removeLast()
+                    result.append(last.lowerBound...max(last.upperBound, range.upperBound))
+                } else {
+                    result.append(range)
+                }
+            }
+            .map { $0.upperBound - $0.lowerBound + 1 }
+            .sum
     }
 }
