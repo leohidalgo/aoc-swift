48f3af6b310341bc07a93e477a0d2673b50f57f0
leohidalgo <leohidalgo@protonmail.com>
2025-12-10 14:37:36 -0300

    feat(aoc-2025): add Day 06 (part 2) (#61)

--- Sources/AOC2025/Day06.swift
+++ Sources/AOC2025/Day06.swift
@@ -25,6 +25,39 @@ struct Day06: Day {
     }
 
     func part2() throws -> Int {
-        -1
+        let board = input().lines.map(\.characters)
+
+        let maxLength = board.reduce(0) { result, line in
+            max(result, line.count)
+        }
+
+        let numbers = (0..<maxLength)
+            .map { x in
+                board.reduce(0) { number, line in
+                    guard
+                        x < line.count,
+                        let digit = line[x].wholeNumberValue
+                    else { return number }
+
+                    return number * 10 + digit
+                }
+            }
+            .split(separator: 0)
+
+        let operations = board.last?.filter { !$0.isWhitespace } ?? []
+
+        return zip(numbers, operations)
+            .map { numbers, operation  in
+                if operation == "*" {
+                    return numbers.reduce(1, *)
+                }
+
+                if operation == "+" {
+                    return numbers.reduce(0, +)
+                }
+
+                return 0
+            }
+            .sum
     }
 }
